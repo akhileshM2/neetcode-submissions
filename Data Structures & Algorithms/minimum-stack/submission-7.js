3d2204524a0db29f7d15ec8a3b64a3edf86c3791
Akhilesh Mishra <107499684+akhileshM2@users.noreply.github.com>
2026-06-21 18:03:55 +0530

    Add: minimum-stack - submission-7

---
+++ Data Structures & Algorithms/minimum-stack/submission-7.js
@@ -0,0 +1,48 @@
+class MinStack {
+    constructor() {
+       this.stack = new Array()
+       this.minValue = Infinity
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        // if (this.stack.length === 0) {
+        //     this.stack.push(0)
+        //     this.minValue = val
+        // } else {
+        //     this.stack.push(val - this.minValue)
+        //     if (val < this.minValue) this.minValue = val
+        // }
+        if (this.stack.length === 0) {
+            this.minValue = Infinity 
+        }
+        this.minValue = Math.min(this.minValue, val)
+        this.stack.push([val, this.minValue])
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        const top = this.stack.pop()[0]
+        if (this.stack.length > 0) this.minValue = this.stack[this.stack.length - 1][1]
+        else this.minValue = top[1]
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length - 1][0]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.stack[this.stack.length - 1][1]
+    }
+}
