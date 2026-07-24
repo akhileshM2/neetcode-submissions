4bac71e4fd35e33773f83bc7d3005bced1e19973
Akhilesh Mishra <107499684+akhileshM2@users.noreply.github.com>
2026-07-24 20:38:40 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    subsets(nums) {
+        let res = []
+        function dfs(i, subset) {
+            if (i >= nums.length) {
+                res.push([...subset])
+                return
+            }
+
+            subset.push(nums[i])
+            dfs(i + 1, subset)
+            subset.pop()
+            dfs(i + 1, subset)
+        }
+
+        dfs(0, [])
+        return res
+    }
+}
