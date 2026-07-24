1d61d0aea8b70a8e005f34ee0adfeba7c9f51f59
Akhilesh Mishra <107499684+akhileshM2@users.noreply.github.com>
2026-07-24 20:52:03 +0530

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @returns {number[][]}
+     */
+    combinationSum(nums, target) {
+        let res = []
+        function dfs(i, comb, sum) {
+            if (sum === target) {
+                res.push([...comb])
+                return
+            }
+
+            if (i >= nums.length) {
+                return
+            }
+
+            if (sum < target) {
+                comb.push(nums[i])
+                sum += nums[i]
+            } else if (sum > target) {
+                return
+            }
+
+            dfs(i, comb, sum)
+            comb.pop()
+            sum -= nums[i]
+            dfs(i + 1, comb, sum)
+        }
+
+        dfs(0, [], 0)
+        return res
+    }
+}
