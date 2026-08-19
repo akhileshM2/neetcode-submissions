4d1f14453bb0be5f4cd53e6e65326de6789cc21b
Akhilesh Mishra <107499684+akhileshM2@users.noreply.github.com>
2026-08-19 23:57:26 +0530

    Add: asteroid-collision - submission-5

---
+++ Data Structures & Algorithms/asteroid-collision/submission-5.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} asteroids
+     * @return {number[]}
+     */
+    asteroidCollision(asteroids) {
+        let stack = []
+
+        for (let a of asteroids) {
+            while (stack.length && a < 0 && stack.at(-1) > 0) {
+                const diff = a + stack.at(-1)
+                if (diff < 0) {
+                    stack.pop()
+                } else if (diff > 0) {
+                    a = 0
+                } else {
+                    a = 0
+                    stack.pop()
+                }
+            }
+            if (a !== 0) {
+                stack.push(a)
+            }
+        }
+        return stack
+    }
+}
