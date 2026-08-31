ad07be1277260b64912cd7ab7a0286b4f6a5bd13
Akhilesh Mishra <107499684+akhileshM2@users.noreply.github.com>
2026-08-31 22:59:28 +0530

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    sortColors(nums) {
+        let count = new Map()
+        for (const n of nums) {
+            count.set(n, (count.get(n) || 0) + 1)
+        }
+
+        let idx = 0
+        for (let i = 0; i < 3; i++) {
+            while (count.get(i) > 0) {
+                nums[idx++] = i
+                count.set(i, count.get(i) - 1)
+            }
+        }
+    }
+}
