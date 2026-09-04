527cec7daa4e778cb4b9262d40bd31a5c9fbfcc9
Akhilesh Mishra <107499684+akhileshM2@users.noreply.github.com>
2026-09-04 20:21:25 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-4

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-4.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let res = 0
+        for (let i = 1; i < prices.length; i++) {
+            let profit = prices[i] - prices[i - 1]
+            if (profit > 0) {
+                res += profit
+            }
+        }
+        return res
+    }
+}
