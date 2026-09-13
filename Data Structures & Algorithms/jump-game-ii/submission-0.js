9661ca1be5f98010bf86505229583f1677c2886e
Akhilesh Mishra <107499684+akhileshM2@users.noreply.github.com>
2026-09-13 17:49:11 +0530

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    jump(nums) {
+        let res = 0, l = 0, r = 0
+
+        while (r < nums.length - 1) {
+            let far = 0
+            for (let i = l; i <= r; i++) {
+                far = Math.max(far, i + nums[i])
+            }
+            l = r + 1
+            r = far
+            res += 1
+        }
+        return res
+    }
+}
